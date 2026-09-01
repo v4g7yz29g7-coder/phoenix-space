196c1dc8191ef03465faf757d4c528b50846e155
Phoenix Architect <architect@phoenix.space>
2026-09-01 16:00:20 +0000

    Add generated test agent module

---
+++ lib/phoenix_app/test_agent.ex
@@ -0,0 +1,20 @@
+
+defmodule PhoenixApp.TestAgent do
+  @moduledoc """
+  Тестовый агент для Phoenix приложения.
+  """
+
+  @doc """
+  Возвращает атом :ok.
+
+  ## Примеры
+
+      iex> PhoenixApp.TestAgent.hello()
+      :ok
+
+  """
+  def hello do
+    :ok
+  end
+end
+
