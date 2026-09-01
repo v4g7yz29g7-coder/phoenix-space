a4f19b9a85944e6643a3bfcc6e1c8294eda81937
Phoenix Architect <architect@phoenix.space>
2026-09-01 04:54:09 +0000

    chore: update runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :phoenix_app, PhoenixAppWeb.Endpoint, server: true
+  config :phoenix_app, PhoenixAppWeb.Endpoint, ip: {0, 0, 0, 0}, server : true
 end
 
 if config_env() == :dev do
